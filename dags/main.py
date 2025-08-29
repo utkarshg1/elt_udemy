95b1d0bcc3a8d933fc9ea4b4fbbb08a96223c9ef
Utkarsh Gaikwad <109385199+utkarshg1@users.noreply.github.com>
2025-08-29 19:53:57 +0000

    updated dag

--- dags/main.py
+++ dags/main.py
@@ -6,5 +6,12 @@ from api.video_stats import get_playlist_id, get_video_ids, get_video_data, save
 local_tz = pendulum.timezone("Asia/Kolkata")
 
 default_args = {
-    "owner": "dataengineers"
+    "owner": "dataengineers",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "email": "data@engineers.com",
+    "max_active_runs": 1,
+    "dagrun_timeout": timedelta(hours=1),
+    "start_date": datetime(2025, 1, 1, tzinfo=local_tz)
 }
