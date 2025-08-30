54c9e0cba5887cc2bdcf45061323b932304bb8c9
Utkarsh Gaikwad <109385199+utkarshg1@users.noreply.github.com>
2025-08-30 13:53:11 +0000

    updated

--- dags/main.py
+++ dags/main.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 import pendulum
 from datetime import datetime, timedelta
-from api.video_stats import get_playlist_id, get_video_ids, get_video_data, save_to_json
+from api.video_stats import get_playlist_id, get_video_ids, get_video_data, save_to_json, channel_handle
 
 local_tz = pendulum.timezone("Asia/Kolkata")
 
@@ -15,3 +15,17 @@ default_args = {
     "dagrun_timeout": timedelta(hours=1),
     "start_date": datetime(2025, 1, 1, tzinfo=local_tz)
 }
+
+with DAG(
+    dag_id="produce_json",
+    default_args=default_args,
+    description="DAG to produce JSON file with raw data",
+    schedule="0 14 * * *",
+    catchup=False
+) as dag:
+    playlist_id = get_playlist_id(channel_handle)
+    video_ids = get_video_ids(playlist_id)
+    video_data = get_video_data(video_ids)
+    save_to_json_task = save_to_json(video_data)
+
+    playlist_id >> video_ids >> video_data >> save_to_json_task
